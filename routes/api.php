5033f39f115def0a0b04986f0a1f3b64bf697db8
Roland John Hontalba <143258252+Roland-John@users.noreply.github.com>
2023-12-21 14:40:21 +0800

    Added User Specific Update Image and Profile Information Show

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\CarouselItemsController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
-
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\CarouselItemsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +25,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     
     Route::post('/logout', [AuthController::class, 'logout']);
 
+    //Admin APIs
     Route::controller(CarouselItemsController::class)->group(function () {
         Route::get('/carousel',         'index');
         Route::post('/carousel',        'store');
@@ -34,6 +34,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::delete('/carousel/{id}', 'destroy');    
     });
 
+    //Admin APIs
     Route::controller(UserController::class)->group(function () {
         Route::get('/user',                 'index');
         Route::get('/user/{id}',            'show');
@@ -41,8 +42,14 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/user',                'store')->name('user.store');
         Route::put('/user/{id}',            'update')->name('user.update');
         Route::put('/user/email/{id}',      'email')->name('user.email');
+        Route::put('/user/image/{id}',      'image')->name('user.image');
         Route::put('/user/password/{id}',   'password')->name('user.password');    
     });
+
+    //User Specific APIs
+    Route::get('/profile/show',     [ProfileController::class, 'show']);
+    Route::put('/profile/image',    [ProfileController::class, 'image'])->name('profile.image');
+    
 });
 
 
